6560d3411fd9c36c7b64ff7e85bec897308e604d
Madhu <114853422+MadhuSuniL@users.noreply.github.com>
2025-09-01 17:33:13 +0530

    Update Images.jsx

--- src/assets/projects/Images.jsx
+++ src/assets/projects/Images.jsx
@@ -1,7 +1,6 @@
 import AI_AGENTS from './ai_agents.jpg';
 import GPT_SUITE from './chat_gpt.jpg';
 import CUSTOM_GPT from './custom_gpts.jpg';
-import PERSONA_BUILDER from './persona_builder.jpg';
 import PMS from './pms.png';
 import CRM from './crm.png';
 import USER_MANAGEMENT from './user_management.jpg';
@@ -9,16 +8,11 @@ import CC_FRAUD from './credit_card.jpg';
 import DYNAMIC_PRICING from './center_price.jpg';
 import RESUME_PARSER from './resume_parser.jpg';
 import FACE_MASK from './musk_detection.jpg';
-import VISUAL_SEARCH from './visual_search.jpg';
-import SEQ2SEQ_CHATBOT from './chat_bot.jpg'; // You can use another if available
-import TEXT_TO_IMAGE from './text_img.jpg';
-import SPEECH_EMOTION from './sound_rec.jpg';
 
 const Images = {
     AI_AGENTS,
     GPT_SUITE,
     CUSTOM_GPT,
-    PERSONA_BUILDER,
     PMS,
     CRM,
     USER_MANAGEMENT,
@@ -26,10 +20,6 @@ const Images = {
     DYNAMIC_PRICING,
     RESUME_PARSER,
     FACE_MASK,
-    VISUAL_SEARCH,
-    SEQ2SEQ_CHATBOT,
-    TEXT_TO_IMAGE,
-    SPEECH_EMOTION,
 };
 
 export default Images;
